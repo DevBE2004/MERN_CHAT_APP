6945f88a50971b7c9d696eb3e8d94c65b6841546
Anh Tu <leantu2004@gmail.com>
2026-01-09 15:14:41 +0700

    push

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,64 +1,64 @@
-import "dotenv/config";
-import path from "path";
-import express, { Request, Response } from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import http from "http";
-import passport from "passport";
-import { Env } from "./config/env.config";
-import { asyncHandler } from "./middlewares/asyncHandler.middleware";
-import { HTTPSTATUS } from "./config/http.config";
-import { errorHandler } from "./middlewares/errorHandler.middleware";
-import connectDatabase from "./config/database.config";
-import { initializeSocket } from "./lib/socket";
-import routes from "./routes";
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import 'dotenv/config'
+import express, { Request, Response } from 'express'
+import http from 'http'
+import passport from 'passport'
+import path from 'path'
+import connectDatabase from './config/database.config'
+import { Env } from './config/env.config'
+import { HTTPSTATUS } from './config/http.config'
+import { initializeSocket } from './lib/socket'
+import { asyncHandler } from './middlewares/asyncHandler.middleware'
+import { errorHandler } from './middlewares/errorHandler.middleware'
+import routes from './routes'
 
-import "./config/passport.config";
+import './config/passport.config'
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
 
 //socket
-initializeSocket(server);
+initializeSocket(server)
 
-app.use(express.json({ limit: "10mb" }));
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }))
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
 app.use(
   cors({
-    origin: Env.FRONTEND_ORIGIN,
+    origin: [Env.FRONTEND_ORIGIN],
     credentials: true,
-  })
-);
+  }),
+)
 
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 app.get(
-  "/health",
+  '/health',
   asyncHandler(async (req: Request, res: Response) => {
     res.status(HTTPSTATUS.OK).json({
-      message: "Server is healthy",
-      status: "OK",
-    });
-  })
-);
+      message: 'Server is healthy',
+      status: 'OK',
+    })
+  }),
+)
 
-app.use("/api", routes);
+app.use('/api', routes)
 
-if (Env.NODE_ENV === "production") {
-  const clientPath = path.resolve(__dirname, "../../client/dist");
+if (Env.NODE_ENV === 'production') {
+  const clientPath = path.resolve(__dirname, '../../client/dist')
 
   //Serve static files
-  app.use(express.static(clientPath));
+  app.use(express.static(clientPath))
 
   app.get(/^(?!\/api).*/, (req: Request, res: Response) => {
-    res.sendFile(path.join(clientPath, "index.html"));
-  });
+    res.sendFile(path.join(clientPath, 'index.html'))
+  })
 }
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 server.listen(Env.PORT, async () => {
-  await connectDatabase();
-  console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
-});
+  await connectDatabase()
+  console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`)
+})
