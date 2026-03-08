81ab4862c5aa0a5903843eb7ba20d1c98668612d
Anh Tu <leantu2004@gmail.com>
2026-03-09 00:28:45 +0700

    push

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 
 const corsOptions = {
-  origin: Env.FRONTEND_ORIGIN,
+  origin: 'https://mern-chat-app-seven-gamma.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }
